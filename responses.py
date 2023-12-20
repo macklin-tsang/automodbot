43626acb55578b0811286e6e1f4a7000a130f04f
macklin-tsang <110071654+macklin-tsang@users.noreply.github.com>
2023-12-19 21:18:26 -0800

    Update responses.py

--- responses.py
+++ responses.py
@@ -4,5 +4,3 @@ def handle_message(response) -> str:
     if pMessage == 'dirk':
         return 'quiet on ya block in some timbs when i lurk,\n glock shots got you on one foot fallin back like you Dirk'
     
-    if pMessage == 'bitchass':
-        return 'ballsack'
